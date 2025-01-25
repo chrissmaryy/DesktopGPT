01f8bbd0801a3c623d872f00e143aaee08d9af21
chrissmaryy <christina@familie-schardt.net>
2025-01-25 21:19:42 +0100

    Added basic SQL logic

---
+++ DesktopGPT/Data/ChatRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopGPT.Data
+{
+    public class ChatRepository
+    {
+        public static void InsertChat(string chat_name)
+        {
+            string query = @$"
+                INSERT INTO Chats
+                (
+                    chat_name
+                )
+                VALUES
+                (
+                    @chat_name
+                );
+            ";
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "@chat_name", chat_name }
+            };
+
+            DatabaseManager.ExecuteNonQuery(query, parameters);
+        }
+
+        public static void UpdateChat(string chat_name, int chat_id)
+        {
+            string query = @$"
+                UPDATE Chats
+                SET
+                    chat_name = @chat_name
+                WHERE chat_id = @chat_id;
+            ";
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "@chat_name", chat_name },
+                { "@chat_id", chat_id }
+            };
+
+            DatabaseManager.ExecuteNonQuery(query, parameters);
+        }
+
+        public static List<Dictionary<string, object>> GetChatList()
+        {
+            string query = @$"
+                SELECT chat_id_ chat_name
+                FROM Chats
+            ";
+
+            List<Dictionary<string, object>> chat_list = DatabaseManager.ExecuteReader(query);
+
+            return chat_list;
+        }
+
+        public static void DeleteChat(int chat_id)
+        {
+            MessageRepository.DeleteMessages(chat_id);
+
+            string query = @$"
+                DELETE FROM Chats
+                WHERE chat_id = {chat_id};
+            ";
+
+            DatabaseManager.ExecuteNonQuery(query);
+        }
+    }
+}
--- DesktopGPT/Data/MessageRepository.cs
+++ DesktopGPT/Data/MessageRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity.Infrastructure;
 using System.Data.SQLite;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace DesktopGPT.Data
     public class MessageRepository
     {
 
-        public static void InsertMessage(int message_id, int chat_id, string model, string role, string content, SQLiteConnection connection)
+        public static void InsertMessage(int message_id, int chat_id, string model, string role, string content, DateTime timestamp)
         {
             string query = @$"
                 INSERT INTO Messages
@@ -21,19 +22,68 @@ namespace DesktopGPT.Data
                     chat_id,
                     model,
                     role,
-                    content
+                    content,
+                    timestamp
                 )
                 VALUES
                 (
-                    '{message_id}',
-                    '{chat_id}',
-                    '{model}',
-                    '{role}',
-                    '{content}'
+                    @message_id,
+                    @chat_id,
+                    @model,
+                    @role,
+                    @content,
+                    @timestamp
                 );
             ";
 
+            var parameters = new Dictionary<string, object>
+            {
+                { "@message_id", message_id },
+                { "@chat_id", chat_id },
+                { "@model", model },
+                { "@role", role },
+                { "@content", content },
+                { "@timestamp", timestamp }
+            };
+
+            DatabaseManager.ExecuteNonQuery(query, parameters);
+        }
+
+        public static List<Dictionary<string, object>> GetMessages(int chat_id)
+        {
+            string query = @$"
+                SELECT content, role, timestamp
+                FROM Messages
+                WHERE chat_id = {chat_id};
+            ";
+
+            List<Dictionary<string, object>> messages_list = DatabaseManager.ExecuteReader(query);
+
+            return messages_list;
+        }
+
+        public static void DeleteMessages(int chat_id)
+        {
+            string query = @$"
+                DELETE FROM Messages
+                WHERE chat_id = {chat_id};
+            ";
+
             DatabaseManager.ExecuteNonQuery(query);
+        }
+
+
+        public static List<Dictionary<string, object>> GetTokenInfo(string model)
+        {
+            string query = @$"
+                SELECT max_tokens, token_buffer
+                FROM Token_Info
+                WHERE model = {model};
+            ";
+
+            List<Dictionary<string, object>> token_info = DatabaseManager.ExecuteReader(query);
+
+            return token_info;
         }
     }
 }
